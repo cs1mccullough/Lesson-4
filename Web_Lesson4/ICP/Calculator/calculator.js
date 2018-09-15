675139818962903d1877bd393d438f613695ff42
cs1mccullough <42701800+cs1mccullough@users.noreply.github.com>
2018-09-15 16:01:36 -0500

    new
    commits to

--- Web_Lesson4/ICP/Calculator/calculator.js
+++ Web_Lesson4/ICP/Calculator/calculator.js
@@ -1,4 +1,31 @@
-angular.module('CalculatorApp', [])
-    .controller('CalculatorController', function ($scope) {
+var app = angular.module('cApp', []);
+cApp.controller('cCon', function ($scope) {
         // Write code simple calculator operations
+
+
+
+
+
     });
+
+var saveval ="0";
+var op;
+
+function inputto(val){
+    document.form1.result.value += val;
+}
+function  clearinput() {
+    document.form1.result.value="";
+}
+
+function add(){
+    saveval=passInt(document.form1.result.value)+passInt(saveval);
+    operator="+";
+    document.form1.result.value="";
+}
+function equalto(){
+    document.form1.result.value=saveval;
+    saveval="0"   }
+
+
+
--- Web_Lesson4/ICP/Todo/todo.js
+++ Web_Lesson4/ICP/Todo/todo.js
@@ -1,23 +1,37 @@
 var angularTodo = angular.module('angularTodo', []);
 
 angularTodo.controller('angularTodoC', ['$scope', function ($scope) {
+
+    //$scope.done = false;
     // define list of items
-    $scope.items = [];
+    $scope.items = ["eat lunch", "learn about cheese", "complete ICP 4"];
 
     // Write code to push new item
 
     $scope.submitNewItem = function () {
+        $scope.items.push($scope.submitNew);
 
     };
 
     // Write code to complete item
-    $scope.completeItem = function (index) {
 
-    };
+    $scope.completeItem = function (index) {
+       // $scope.done = !$scope.done;
+        $scope.items[index].toggleClass(index, 'li', donezo);
+
+        };
+        //[index].toggle('doner');
+    //};
+
 
     // Write code to delete item
 
     $scope.deleteItem = function (index) {
-
+        $scope.items.splice(index, 1);
     };
 }]);
+
+
+$scope.myFunc = function() {
+
+}
